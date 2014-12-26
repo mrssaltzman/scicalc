7d4a849767b2c433a26585430415848d33ab7886
mrssaltzman <cydakya@outlook.com>
2014-12-26 16:44:49 +0200

    yroot fonksiyonu eklendi

--- scicalc/Form1.Designer.cs
+++ scicalc/Form1.Designer.cs
@@ -65,7 +65,7 @@
             this.button_powten = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.button_tanh = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.button_yroot = new System.Windows.Forms.Button();
             this.button_fact = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
@@ -455,14 +455,15 @@
             this.button_tanh.UseVisualStyleBackColor = true;
             this.button_tanh.Click += new System.EventHandler(this.button_tanh_Click);
             // 
-            // button3
+            // button_yroot
             // 
-            this.button3.Location = new System.Drawing.Point(325, 302);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(56, 54);
-            this.button3.TabIndex = 16;
-            this.button3.Text = "karakök xy";
-            this.button3.UseVisualStyleBackColor = true;
+            this.button_yroot.Location = new System.Drawing.Point(325, 302);
+            this.button_yroot.Name = "button_yroot";
+            this.button_yroot.Size = new System.Drawing.Size(56, 54);
+            this.button_yroot.TabIndex = 16;
+            this.button_yroot.Text = "karakök xy";
+            this.button_yroot.UseVisualStyleBackColor = true;
+            this.button_yroot.Click += new System.EventHandler(this.button_yroot_Click);
             // 
             // button_fact
             // 
@@ -656,7 +657,7 @@
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button_fact);
-            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button_yroot);
             this.Controls.Add(this.button_tanh);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.button_powten);
@@ -740,7 +741,7 @@
         private System.Windows.Forms.Button button_powten;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button_tanh;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button_yroot;
         private System.Windows.Forms.Button button_fact;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button6;
